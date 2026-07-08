d8e90c6e11cfe6510c01bdad1f2821cfb5561215
Jonathan Melgar <amelgar08@gmail.com>
2026-07-08 19:13:36 +0000

    Added feature to write assembly instruction program instead of needing instruction hex

--- sim/run.sh
+++ sim/run.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+set -e
 
 echo "Cleaning old build.."
 rm -rf obj_dir
 
-echo "Compiling..."
+echo "Assembling program..."
+python3 ../tools/asm_to_hex.py ../programs/full_instruction_test.s program.hex
 
+echo "Compiling..."
 verilator -Wall --I../rtl --I../tb --timing -Wno-fatal --cc ../rtl/*.sv ../tb/tb_cpu.sv --top-module tb_cpu --exe sim_main.cpp
 
+echo "Building..."
 make -C obj_dir -f Vtb_cpu.mk Vtb_cpu
 
 echo "Running simulation..."
-
 ./obj_dir/Vtb_cpu
